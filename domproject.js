e26d320325f5e325fb188ba8ea128970d6b734c9
Jamie Stewart <stwjme@gmail.com>
2020-05-21 16:56:51 -0400

    TitlesArray

--- domproject.js
+++ domproject.js
@@ -2,3 +2,7 @@ let search = document.getElementById('search-books');
 let bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
